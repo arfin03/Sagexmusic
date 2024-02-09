f781831a1db7b9d5f3382a6fb23af94ecbbbc558
Sagexdd <159287148+Sagexdd@users.noreply.github.com>
2024-02-09 19:49:56 +0530

    Update repo.py

--- AloneXMusic/plugins/bot/repo.py
+++ AloneXMusic/plugins/bot/repo.py
@@ -4,7 +4,7 @@ from AloneXMusic import app
 from config import BOT_USERNAME
 
 start_txt = """
-✪ 𝐖εℓ¢σмє 𝐅σя 𝐀ℓσиє 𝐑єρσѕ ✪
+✪ ᴡᴇʟᴄᴏᴍᴇ ғᴏʀ sᴀɢᴡ ʀᴇᴘᴏ ✪
  
  ➲ ᴀʟʟ ʀᴇᴘᴏ ᴇᴀsɪʟʏ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴀɴʏ ᴇʀʀᴏʀ ✰
  
@@ -26,50 +26,30 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔𝗗𝗗 𝗠𝗘", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/AlonesHeaven"),
-          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/ALONE_WAS_BOT"),
+          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/sagebotsupport"),
+          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/unfav_sage"),
           ],
                [
-                InlineKeyboardButton("𝗔𝗟𝗢𝗡𝗘 𝗡𝗘𝗧𝗪𝗢𝗥𝗞", url=f"https://t.me/AloneXBots"),
+                InlineKeyboardButton("𝗦𝗔𝗚𝗘 𝗡𝗘𝗧𝗪𝗢𝗥𝗞", url=f"https://t.me/sagebotsupport"),
 
 ],
-[
-              InlineKeyboardButton("𝗩1 𝗠𝗨𝗦𝗜𝗖", url=f"https://github.com/TeamAloneOp/AloneX"),
-              InlineKeyboardButton("𝗩2 𝗠𝗨𝗦𝗜𝗖", url=f"https://github.com/TeamAloneOp/AloneXMusic"),
+             [
+              InlineKeyboardButton("𝗩2 𝗠𝗨𝗦𝗜𝗖", url=f"https://github.com/Sagexdd/Sagexmusic"),
               ],
               [
-              InlineKeyboardButton("𝗩1 𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧", url=f"https://github.com/TeamAloneOp/AloneXRobot"),
-InlineKeyboardButton("𝗩2 𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧", url=f"https://github.com/TeamAloneOp/AloneRobot"),
+InlineKeyboardButton("𝗩2 𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧", url=f"https://github.com/Sagexdd/Sagexmusic"),
 ],
+
+
 [
-InlineKeyboardButton("𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 𝗠𝗨𝗦𝗜𝗖", url=f"https://github.com/TeamAloneOp/Telethon-Music"),
-InlineKeyboardButton("𝗔𝗟𝗢𝗡𝗘 𝗬𝗨𝗞𝗞𝗜", url=f"https://github.com/TeamAloneOp/AloneMusicBot"),
-],
-[
-InlineKeyboardButton("𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/TeamAloneOp/AloneXChatBot"),
-InlineKeyboardButton("𝗜𝗗 𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/TeamAloneOp/AloneXIdChatbot"),
-],
-[
-              InlineKeyboardButton("𝗦𝗣𝗔𝗠", url=f"https://github.com/TeamAloneOp/AloneXSpam"),
-              InlineKeyboardButton("𝗕𝗔𝗡𝗔𝗟𝗟", url=f"https://github.com/TeamAloneOp/AloneXBanallBot"),
-              ],
-              [
-              InlineKeyboardButton("𝗦𝗧𝗥𝗜𝗡𝗚𝗕𝗢𝗧", url=f"https://github.com/TeamAloneOp/AloneXStringGenBot"),
-InlineKeyboardButton("𝗕𝗬𝗣𝗔𝗦𝗦", url=f"https://github.com/TeamAloneOp/AloneXBypass"),
-],
-[
-InlineKeyboardButton("𝗥𝗪 𝗦𝗧𝗥𝗜𝗣𝗘", url=f"https://github.com/TeamAloneOp/kaali-Linux"),
-InlineKeyboardButton("𝗥𝗪 𝗗𝗘𝗣𝗟𝗢𝗬", url=f"https://github.com/TeamAloneOp/AloneXRailway"),
-],
-[
-InlineKeyboardButton("𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗕𝗢𝗧", url=f"https://t.me/AloneXMusicBot"),
+InlineKeyboardButton("𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗕𝗢𝗧", url=f"https://t.me/sagexmusicbot"),
 
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://telegra.ph/file/70402820cd4afd8ea4c13.jpg",
+        photo="https://telegra.ph/file/9179ebe031879224ac049.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
